ae34f988aa508fc953f1545feacb21edce91019f
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2022-01-24 22:00:23 -0300

    inicio de projeto

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+
+import Head from "next/head";
+
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+    <title>natcults</title>
+    </Head>
+    <h1>Hellow world</h1>
+    </>
+  )
+}
